02ff9aa28a9e6bdd87b30dd90144484e2d77d4f4
Scott Munro <scottnmunro@gmail.com>
2019-02-17 00:14:46 -0800

    maybe cocky but it feels like progress to getting an iterator type

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <cstdio>
 #include <vector>
 #include <iterator>
+#include <type_traits>
+#include <iostream>
 
-template <typename TIterable, typename TIterator, typename TPredicate>
+/*
+template <typename TIterable, typename TPredicate>
 class filter_iter
 {
 public:
+    typename typedef std::conditional<
+        std::enable_if<std::is_array<TIterable>>,
+        std::add_pointer<std::remove_extent<TIterable>>,
+        TIterable::iterator>::type TIterator;
+
     typename typedef std::iterator_traits<TIterator>::value_type value_type;
     typename typedef std::iterator_traits<TIterator>::reference reference;
     typename typedef std::iterator_traits<TIterator>::pointer pointer;
@@ -63,42 +71,82 @@ private:
     const TPredicate pred;
 };
 
-/*
-template <typename TIterable, typename TIterator, typename TDerefed, typename TPredicate>
-filter_iter<TIterable, TIterator, TDerefed, TPredicate>
-f_filter_iter(const TIterable& iter, TPredicate& pred)
-{
-    return filter_iter<TIterable, TIterator, TDerefed, TPredicate>{iter, pos};
-}
-*/
-
 bool IsEven(int v) { return (v & 1) == 0; }
 
 void test1()
 {
     int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto iter = filter_iter<int(&)[11], int*, bool(*)(int)>(values, IsEven);
+    auto iter = filter_iter<int(&)[11], bool(*)(int)>(values, IsEven);
     for (const int& i : iter)
     {
         printf("%i ", i);
     }
     printf("\nDone\n");
 }
-
-/*
-void test2()
-{
-    int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto iter = f_filter_iter(values, IsEven);
-    for (const int& i : iter)
-    {
-        printf("%i ", i);
-    }
-    printf("\nDone\n");
-}
-*/
 
 int main()
 {
     test1();
 }
+*/
+
+template<typename T>
+struct iterator_for
+{
+    typedef typename T::iterator  type;
+};
+
+template<class T>
+struct iterator_for<T*>
+{
+    typedef T*  type;
+};
+
+template <typename T, size_t N>
+struct iterator_for<T[N]>
+{
+    typedef T*  type;
+};
+
+template<typename T>
+void
+PrintTypes(const T& v)
+{
+    /*
+    typename typedef std::conditional<
+        std::is_array<T>::value,
+        std::decay<T>::type,
+        std::iterator_traits<T>::iterator>::type conditional_type;
+    */
+    typename typedef iterator_for<T>::type iterator_type;
+    std::cout <<  "TYPES" << std::endl;
+    (void)v;
+    /*
+    std::cout << typeid(T).name() << std::endl;
+    std::cout << std::is_array<T>::value << std::endl;
+    std::cout << typeid(std::decay<T>::type).name() << std::endl;
+    */
+    std::cout << typeid(iterator_type).name() << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    int a[5];
+    PrintTypes(a);
+
+    typedef iterator_for<int[5]>::type test_type;
+    test_type i = nullptr;
+
+    std::vector<int> b;
+    PrintTypes(b);
+}
+
+/*
+ *
+ * std::conditional<
+ *      bool B = std::is_array<TIterable>::value,
+ *      class T = std::decay<TIterable>::type,
+ *      class F = TIterable::iterator,
+ *      >
+ */
