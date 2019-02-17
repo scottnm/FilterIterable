9a61aba04b80df5bb69ad78b5e0b373e25ad54f5
Scott Munro <scottnmunro@gmail.com>
2019-02-16 23:24:32 -0800

    got rid of one type to specify

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <cstdio>
 #include <vector>
+#include <iterator>
 
-template <typename TIterable, typename TIterator, typename TDerefed, typename TPredicate>
+template <typename TIterable, typename TIterator, typename TPredicate>
 class filter_iter
 {
 public:
+    typename typedef std::iterator_traits<TIterator>::value_type value_type;
+    typename typedef std::iterator_traits<TIterator>::reference reference;
+    typename typedef std::iterator_traits<TIterator>::pointer pointer;
+    typename typedef std::iterator_traits<TIterator>::difference_type difference_type;
+    typename typedef std::iterator_traits<TIterator>::iterator_category iterator_category;
+
     filter_iter(const TIterable& iter, const TPredicate& pred) :
         iter(iter),
         pred(pred)
@@ -29,10 +36,10 @@ public:
             return *this;
         }
 
-              TDerefed* operator->()       { return i; }
-        const TDerefed* operator->() const { return i; }
-              TDerefed& operator *()       { return *i; }
-        const TDerefed& operator *() const { return *i; }
+              value_type* operator->()       { return i; }
+        const value_type* operator->() const { return i; }
+              value_type& operator *()       { return *i; }
+        const value_type& operator *() const { return *i; }
 
     private:
         TIterator i;
@@ -70,7 +77,7 @@ bool IsEven(int v) { return (v & 1) == 0; }
 void test1()
 {
     int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto iter = filter_iter<int(&)[11], int*, int, bool(*)(int)>(values, IsEven);
+    auto iter = filter_iter<int(&)[11], int*, bool(*)(int)>(values, IsEven);
     for (const int& i : iter)
     {
         printf("%i ", i);
