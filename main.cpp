9a6782b1fb6b0c6d1f8dec95cfe4a4d435e46213
Scott Munro <scottnmunro@gmail.com>
2019-02-17 00:21:57 -0800

    more progress!

--- main.cpp
+++ main.cpp
@@ -4,15 +4,29 @@
 #include <type_traits>
 #include <iostream>
 
-/*
+template<typename T>
+struct iterator_for
+{
+    typedef typename T::iterator  type;
+};
+
+template<class T>
+struct iterator_for<T*>
+{
+    typedef T*  type;
+};
+
+template <typename T, size_t N>
+struct iterator_for<T[N]>
+{
+    typedef T*  type;
+};
+
 template <typename TIterable, typename TPredicate>
 class filter_iter
 {
 public:
-    typename typedef std::conditional<
-        std::enable_if<std::is_array<TIterable>>,
-        std::add_pointer<std::remove_extent<TIterable>>,
-        TIterable::iterator>::type TIterator;
+    typename typedef iterator_for<TIterable>::type TIterator;
 
     typename typedef std::iterator_traits<TIterator>::value_type value_type;
     typename typedef std::iterator_traits<TIterator>::reference reference;
@@ -20,7 +34,7 @@ public:
     typename typedef std::iterator_traits<TIterator>::difference_type difference_type;
     typename typedef std::iterator_traits<TIterator>::iterator_category iterator_category;
 
-    filter_iter(const TIterable& iter, const TPredicate& pred) :
+    filter_iter(TIterable& iter, const TPredicate& pred) :
         iter(iter),
         pred(pred)
     {
@@ -57,17 +71,19 @@ public:
     filter_iter_pos
     begin()
     {
-        return filter_iter_pos(std::begin(iter), pred);
+        filter_iter_pos x(std::begin(iter), pred);
+        return x;
     }
 
     filter_iter_pos
     end()
     {
-        return filter_iter_pos(std::end(iter), pred);
+        filter_iter_pos y(std::end(iter), pred);
+        return y;
     }
 
 private:
-    const TIterable& iter;
+    TIterable& iter;
     const TPredicate pred;
 };
 
@@ -76,7 +92,7 @@ bool IsEven(int v) { return (v & 1) == 0; }
 void test1()
 {
     int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto iter = filter_iter<int(&)[11], bool(*)(int)>(values, IsEven);
+    auto iter = filter_iter<int[11], bool(*)(int)>(values, IsEven);
     for (const int& i : iter)
     {
         printf("%i ", i);
@@ -88,44 +104,15 @@ int main()
 {
     test1();
 }
-*/
 
-template<typename T>
-struct iterator_for
-{
-    typedef typename T::iterator  type;
-};
-
-template<class T>
-struct iterator_for<T*>
-{
-    typedef T*  type;
-};
-
-template <typename T, size_t N>
-struct iterator_for<T[N]>
-{
-    typedef T*  type;
-};
-
+/*
 template<typename T>
 void
 PrintTypes(const T& v)
 {
-    /*
-    typename typedef std::conditional<
-        std::is_array<T>::value,
-        std::decay<T>::type,
-        std::iterator_traits<T>::iterator>::type conditional_type;
-    */
     typename typedef iterator_for<T>::type iterator_type;
     std::cout <<  "TYPES" << std::endl;
     (void)v;
-    /*
-    std::cout << typeid(T).name() << std::endl;
-    std::cout << std::is_array<T>::value << std::endl;
-    std::cout << typeid(std::decay<T>::type).name() << std::endl;
-    */
     std::cout << typeid(iterator_type).name() << std::endl;
     std::cout << std::endl;
 }
@@ -141,12 +128,4 @@ int main()
     std::vector<int> b;
     PrintTypes(b);
 }
-
-/*
- *
- * std::conditional<
- *      bool B = std::is_array<TIterable>::value,
- *      class T = std::decay<TIterable>::type,
- *      class F = TIterable::iterator,
- *      >
- */
+*/
