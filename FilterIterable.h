de281c38424d19a02cfea832f310573131230dea
Scott Munro <scottnmunro@gmail.com>
2021-03-13 10:57:22 -0800

    fix indentation width

--- FilterIterable.h
+++ FilterIterable.h
@@ -10,73 +10,77 @@
 //
 template <typename TIterable, typename TPredicate> class FilteredIterable {
 public:
-  template <typename T> struct IteratorType {
-    typedef typename T::iterator type;
-  };
+    template <typename T> struct IteratorType {
+        typedef typename T::iterator type;
+    };
 
-  template <class T> struct IteratorType<T *> { typedef T *type; };
+    template <class T> struct IteratorType<T *> { typedef T *type; };
 
-  template <typename T, size_t N> struct IteratorType<T[N]> {
-    typedef T *type;
-  };
+    template <typename T, size_t N> struct IteratorType<T[N]> {
+        typedef T *type;
+    };
 
-  typedef typename IteratorType<TIterable>::type TIterator;
-  typedef typename std::iterator_traits<TIterator>::value_type TItem;
+    typedef typename IteratorType<TIterable>::type TIterator;
+    typedef typename std::iterator_traits<TIterator>::value_type TItem;
 
-  FilteredIterable(TIterable &iterable, TPredicate predicate)
-      : iterable(iterable), predicate(predicate) {
+    FilteredIterable(TIterable &iterable, TPredicate predicate)
+        : iterable(iterable), predicate(predicate) {
 #ifdef PRINT_ITER_TYPES
-    printf("iterable: %s\niterator: %s\n", typeid(TIterable).name(),
-           typeid(TIterator).name());
+        printf("iterable: %s\niterator: %s\n", typeid(TIterable).name(),
+               typeid(TIterator).name());
 #endif // PRINT_ITER_TYPES
-  }
-
-  class FilterIterator {
-  public:
-    FilterIterator(const TIterator &current, const TIterator &end,
-                   TPredicate predicate)
-        : current(current), end(end), predicate(predicate) {
-      AdvanceUntilValid();
     }
 
-    FilterIterator &operator++() {
-      ++current;
-      AdvanceUntilValid();
-      return *this;
+    class FilterIterator {
+    public:
+        FilterIterator(const TIterator &current, const TIterator &end,
+                       TPredicate predicate)
+            : current(current), end(end), predicate(predicate) {
+            AdvanceUntilValid();
+        }
+
+        FilterIterator &operator++() {
+            ++current;
+            AdvanceUntilValid();
+            return *this;
+        }
+
+        bool operator==(const FilterIterator &other) {
+            return current == other.current;
+        }
+        bool operator!=(const FilterIterator &other) {
+            return !(*this == other);
+        }
+        TItem *operator->() { return current; }
+        TItem &operator*() { return *current; }
+        const TItem *operator->() const { return current; }
+        const TItem &operator*() const { return *current; }
+
+    private:
+        void AdvanceUntilValid() {
+            while (current != end && !predicate(*current)) {
+                ++current;
+            }
+        }
+
+        TIterator current;
+        const TIterator end;
+        TPredicate predicate;
+    };
+
+    FilterIterator begin() {
+        return FilterIterator(std::begin(iterable), std::end(iterable),
+                              predicate);
     }
 
-    bool operator==(const FilterIterator &other) {
-      return current == other.current;
+    FilterIterator end() {
+        return FilterIterator(std::end(iterable), std::end(iterable),
+                              predicate);
     }
-    bool operator!=(const FilterIterator &other) { return !(*this == other); }
-    TItem *operator->() { return current; }
-    TItem &operator*() { return *current; }
-    const TItem *operator->() const { return current; }
-    const TItem &operator*() const { return *current; }
-
-  private:
-    void AdvanceUntilValid() {
-      while (current != end && !predicate(*current)) {
-        ++current;
-      }
-    }
-
-    TIterator current;
-    const TIterator end;
-    TPredicate predicate;
-  };
-
-  FilterIterator begin() {
-    return FilterIterator(std::begin(iterable), std::end(iterable), predicate);
-  }
-
-  FilterIterator end() {
-    return FilterIterator(std::end(iterable), std::end(iterable), predicate);
-  }
 
 private:
-  TIterable &iterable;
-  TPredicate predicate;
+    TIterable &iterable;
+    TPredicate predicate;
 };
 
 //
@@ -87,7 +91,7 @@ private:
 template <typename TIterable, typename TPredicate>
 FilteredIterable<TIterable, TPredicate> Filter(TIterable &iterable,
                                                TPredicate predicate) {
-  return FilteredIterable<TIterable, TPredicate>{iterable, predicate};
+    return FilteredIterable<TIterable, TPredicate>{iterable, predicate};
 }
 
 #endif // __FILTER_ITERABLE_H__
--- Samples.cpp
+++ Samples.cpp
@@ -14,171 +14,172 @@
 // Define a helper macro to make appending formatted text to a std::string easy.
 // I just don't want to bother with iostream shenanigans. Hurts my eyes.
 #define APPEND_STR(acc, format, ...)                                           \
-  do {                                                                         \
-    char appendStrTmpBuffer[1024];                                             \
-    (void)sprintf_s(appendStrTmpBuffer, format, __VA_ARGS__);                  \
-    acc += appendStrTmpBuffer;                                                 \
-  } while (0)
+    do {                                                                       \
+        char appendStrTmpBuffer[1024];                                         \
+        (void)sprintf_s(appendStrTmpBuffer, format, __VA_ARGS__);              \
+        acc += appendStrTmpBuffer;                                             \
+    } while (0)
 
 bool IsOdd(int v) { return (v & 1) == 1; }
 
 std::string TestIterableConstruction() {
-  std::string result;
+    std::string result;
 
-  int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  auto iter = FilteredIterable<int[11], bool (*)(int)>(values, IsOdd);
-  for (const int &i : iter) {
-    APPEND_STR(result, "%i ", i);
-  }
+    int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto iter = FilteredIterable<int[11], bool (*)(int)>(values, IsOdd);
+    for (const int &i : iter) {
+        APPEND_STR(result, "%i ", i);
+    }
 
-  result.pop_back();
-  return result;
+    result.pop_back();
+    return result;
 }
 
 std::string TestFilterStackArrays() {
-  std::string result;
+    std::string result;
 
-  int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  for (const int &i : Filter(values, IsOdd)) {
-    APPEND_STR(result, "%i ", i);
-  }
+    int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (const int &i : Filter(values, IsOdd)) {
+        APPEND_STR(result, "%i ", i);
+    }
 
-  result.pop_back();
-  return result;
+    result.pop_back();
+    return result;
 }
 
 std::string TestFilterContainerTypes() {
-  std::string result;
+    std::string result;
 
-  std::vector<int> values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  for (const int &i : Filter(values, IsOdd)) {
-    APPEND_STR(result, "%i ", i);
-  }
+    std::vector<int> values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (const int &i : Filter(values, IsOdd)) {
+        APPEND_STR(result, "%i ", i);
+    }
 
-  result.pop_back();
-  return result;
+    result.pop_back();
+    return result;
 }
 
 std::string TestFilterNonRandomAccessIterable() {
-  std::string result;
+    std::string result;
 
-  std::list<int> values;
-  for (uint32_t i = 0; i <= 10; ++i) {
-    values.emplace_back(i);
-  }
+    std::list<int> values;
+    for (uint32_t i = 0; i <= 10; ++i) {
+        values.emplace_back(i);
+    }
 
-  auto iter = Filter(values, IsOdd);
-  for (const int &i : iter) {
-    APPEND_STR(result, "%i ", i);
-  }
+    auto iter = Filter(values, IsOdd);
+    for (const int &i : iter) {
+        APPEND_STR(result, "%i ", i);
+    }
 
-  result.pop_back();
-  return result;
+    result.pop_back();
+    return result;
 }
 
 struct SomeObject {
-  uint32_t i;
+    uint32_t i;
 
-  // Delete copy construction, copy assignment, and move assignment to verify
-  // that no part of the filter performs copies. Leave the move constructor
-  // alone since it's used by the array initializers
-  SomeObject(int i) : i(i) {}
-  SomeObject(SomeObject &&) = default;
-  SomeObject(const SomeObject &) = delete;
-  SomeObject &operator=(const SomeObject &) = delete;
-  SomeObject &operator=(SomeObject &&) = delete;
+    // Delete copy construction, copy assignment, and move assignment to verify
+    // that no part of the filter performs copies. Leave the move constructor
+    // alone since it's used by the array initializers
+    SomeObject(int i) : i(i) {}
+    SomeObject(SomeObject &&) = default;
+    SomeObject(const SomeObject &) = delete;
+    SomeObject &operator=(const SomeObject &) = delete;
+    SomeObject &operator=(SomeObject &&) = delete;
 };
 
 std::string TestFilterByObjectProperty() {
-  std::string result;
+    std::string result;
 
-  std::list<SomeObject> values;
-  for (uint32_t i = 0; i <= 10; ++i) {
-    values.emplace_back(i);
-  }
+    std::list<SomeObject> values;
+    for (uint32_t i = 0; i <= 10; ++i) {
+        values.emplace_back(i);
+    }
 
-  auto iter = Filter(values, [](const SomeObject &so) { return !IsOdd(so.i); });
-  for (const SomeObject &so : iter) {
-    APPEND_STR(result, "so(%i) ", so.i);
-  }
+    auto iter =
+        Filter(values, [](const SomeObject &so) { return !IsOdd(so.i); });
+    for (const SomeObject &so : iter) {
+        APPEND_STR(result, "so(%i) ", so.i);
+    }
 
-  result.pop_back();
-  return result;
+    result.pop_back();
+    return result;
 }
 
 std::string TestFilterByUniqueObject() {
-  std::string result;
+    std::string result;
 
-  SomeObject values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    SomeObject values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-  const SomeObject &so3 = values[3];
-  auto iter =
-      Filter(values, [&so3](const SomeObject &so) { return &so != &so3; });
-  for (const SomeObject &so : iter) {
-    APPEND_STR(result, "so(%i) ", so.i);
-  }
+    const SomeObject &so3 = values[3];
+    auto iter =
+        Filter(values, [&so3](const SomeObject &so) { return &so != &so3; });
+    for (const SomeObject &so : iter) {
+        APPEND_STR(result, "so(%i) ", so.i);
+    }
 
-  result.pop_back();
-  return result;
+    result.pop_back();
+    return result;
 }
 
 template <typename T>
 std::function<bool(const T &)> Excluder(const T &elementToExclude) {
-  return [&elementToExclude](const T &t) { return &t != &elementToExclude; };
+    return [&elementToExclude](const T &t) { return &t != &elementToExclude; };
 }
 
 std::string TestFilterWithHigherOrderFunctions() {
-  std::string result;
+    std::string result;
 
-  SomeObject values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  for (const SomeObject &so : Filter(values, Excluder(values[4]))) {
-    APPEND_STR(result, "so(%i) ", so.i);
-  }
+    SomeObject values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (const SomeObject &so : Filter(values, Excluder(values[4]))) {
+        APPEND_STR(result, "so(%i) ", so.i);
+    }
 
-  result.pop_back();
-  return result;
+    result.pop_back();
+    return result;
 }
 
 int main() {
-  typedef std::string (*testfunc)(void);
+    typedef std::string (*testfunc)(void);
 
-  struct Test {
-    const char *name;
-    testfunc func;
-    std::string expectedResult;
-  };
+    struct Test {
+        const char *name;
+        testfunc func;
+        std::string expectedResult;
+    };
 
-  Test tests[] = {
-      {"IterableConstruction", TestIterableConstruction, "1 3 5 7 9"},
-      {"FilterStackArrays", TestFilterStackArrays, "1 3 5 7 9"},
-      {"FilterContainerTypes", TestFilterContainerTypes, "1 3 5 7 9"},
-      {"FilterNonRandomAccessIterable", TestFilterNonRandomAccessIterable,
-       "1 3 5 7 9"},
-      {"FilterByObjectProperty", TestFilterByObjectProperty,
-       "so(0) so(2) so(4) so(6) so(8) so(10)"},
-      {"FilterByUniqueObject", TestFilterByUniqueObject,
-       "so(0) so(1) so(2) so(4) so(5) so(6) so(7) so(8) so(9) so(10)"},
-      {"FilterWithHigherOrderFunctions", TestFilterWithHigherOrderFunctions,
-       "so(0) so(1) so(2) so(3) so(5) so(6) so(7) so(8) so(9) so(10)"}};
+    Test tests[] = {
+        {"IterableConstruction", TestIterableConstruction, "1 3 5 7 9"},
+        {"FilterStackArrays", TestFilterStackArrays, "1 3 5 7 9"},
+        {"FilterContainerTypes", TestFilterContainerTypes, "1 3 5 7 9"},
+        {"FilterNonRandomAccessIterable", TestFilterNonRandomAccessIterable,
+         "1 3 5 7 9"},
+        {"FilterByObjectProperty", TestFilterByObjectProperty,
+         "so(0) so(2) so(4) so(6) so(8) so(10)"},
+        {"FilterByUniqueObject", TestFilterByUniqueObject,
+         "so(0) so(1) so(2) so(4) so(5) so(6) so(7) so(8) so(9) so(10)"},
+        {"FilterWithHigherOrderFunctions", TestFilterWithHigherOrderFunctions,
+         "so(0) so(1) so(2) so(3) so(5) so(6) so(7) so(8) so(9) so(10)"}};
 
 #define ANSI_RED "\033[31m"
 #define ANSI_RED_BOLD "\033[1;31m"
 #define ANSI_GREEN "\033[32m"
 #define ANSI_RESET "\033[0m"
 
-  bool passed = true;
-  for (const auto &test : tests) {
-    printf("Testing %s... ", test.name);
-    std::string result = test.func();
-    if (result == test.expectedResult) {
-      printf(ANSI_GREEN "PASSED!\n" ANSI_RESET);
-    } else {
-      printf(ANSI_RED_BOLD "FAILED!\n" ANSI_RESET);
-      printf(ANSI_RED "    expected \"%s\", found \"%s\"\n" ANSI_RESET,
-             test.expectedResult.c_str(), result.c_str());
-      passed = false;
+    bool passed = true;
+    for (const auto &test : tests) {
+        printf("Testing %s... ", test.name);
+        std::string result = test.func();
+        if (result == test.expectedResult) {
+            printf(ANSI_GREEN "PASSED!\n" ANSI_RESET);
+        } else {
+            printf(ANSI_RED_BOLD "FAILED!\n" ANSI_RESET);
+            printf(ANSI_RED "    expected \"%s\", found \"%s\"\n" ANSI_RESET,
+                   test.expectedResult.c_str(), result.c_str());
+            passed = false;
+        }
     }
-  }
 
-  return passed ? 0 : 1;
+    return passed ? 0 : 1;
 }
